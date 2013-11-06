19cfb4c9a831f8361028325bfe9db5efbb20a8d8
nyakahiro <nakahiro@mail.ru>
2013-11-06 18:47:41 +0700

    Исправление критичности

--- classes/modules/user/entity/User.entity.class.php
+++ classes/modules/user/entity/User.entity.class.php
@@ -11,7 +11,7 @@ class PluginRandavatar_ModuleUser_EntityUser extends PluginRandavatar_Inherit_Mo
 			}
 			
             if ($sPath = $this->User_UploadRandAvatar($sRandPath, $this)){
-                return str_replace('_100x100',(($iSize==0)?"":"_{$iSize}x{$iSize}"),$sPath."?".date('His',strtotime($this->getProfileDate())));
+                 return parent::getProfileAvatarPath($iSize);
             }
             return $sRandPath;
         }else{
